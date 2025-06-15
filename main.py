04cfd1839b45695593c571c78ba409d0e6897370
Tomimotmot <reigl.tom@gmail.com>
2025-06-15 14:07:25 +0200

    main.py aktualisieren

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from layout import render_layout
 
+st.set_page_config(page_title="7 Wonders Duel – Zeitalter I", layout="centered")
+
 def main():
-    st.set_page_config(layout="wide")
     render_layout()
 
 if __name__ == "__main__":
     main()
-
