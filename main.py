5e720d1474a984751da4fb69b047285c3d413547
Tomimotmot <reigl.tom@gmail.com>
2025-06-15 14:34:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import streamlit as st
-from layout import render_layout
+from layout import render_layout, render_ressourcen
+from logic import init_game
 
-st.set_page_config(page_title="7 Wonders Duel – Zeitalter I", layout="centered")
+st.set_page_config(page_title="7 Wonders Duel", layout="centered")
 
 def main():
+    init_game()
+    render_ressourcen()
     render_layout()
 
-if __name__ == "__main__":
-    main()
+if __name__ 
